358c051dea84527f25de175c9c9de72fcca9bc9e
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-12-20 10:29:01 +0900

    replace for fluentd plugin style

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-hostname'
+require 'fluent/plugin/out_hostname'
 
 class Test::Unit::TestCase
 end
